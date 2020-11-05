294eac19b9fe4109c40085e8dddcdd03d74dbb3c
Blake <>
2020-11-05 16:02:09 -0600

    finishing out project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Blake McAnally on 11/4/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
